e288aedf4a6976f011771f9678da307fd27737d2
kawonlee <shadow4634@naver.com>
2023-08-18 00:40:34 +0900

    0818 vue study

--- testvue/src/App.vue
+++ testvue/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <TestName />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TestName from "./components/TestName.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    TestName,
+  },
+};
 </script>
 
 <style>
---
+++ testvue/src/components/TestName.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>{{ userName }}</div>
+  <div>{{ msg }}</div>
+  <div v-html="testHtml"></div>
+  <div v-for="item in items" v-bind:key="item">{{ item }}</div>
+  <input type="text" v-model="title" />
+  <div>{{ title }}</div>
+  <div id="test">
+    <h3 v-if="show">Loading ...✔✔✔</h3>
+    <button v-on:click="changeStatus()">눌러봐</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "이가원",
+      msg: "test중입니다.",
+      testHtml: '<span style = "color: red">이건 v-html 사용한 것</span>',
+      items: ["test1", "test2", "test3"],
+      title: "",
+      show: true,
+    };
+  },
+
+  methods: {
+    changeStatus() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  font-size: 20px;
+  color: pink;
+  margin: 10px;
+}
+
+#test {
+  color: blue;
+  font-size: 18px;
+}
+</style>
